0600626d0508f0711880bb13131180fdb8e1a16c
Jacky Xu <jackyxu07@gmail.com>
2018-12-27 15:11:36 -0500

    Configure gtk3.0 theme

--- scripts/configure/configure-desktop.sh
+++ scripts/configure/configure-desktop.sh
@@ -21,9 +21,13 @@ xmlstarlet ed -L -u "//_:resize/_:drawContents" -v "yes" ${HOME}/.config/openbox
 # use the onyx theme as it looks better than the lubuntu theme
 xmlstarlet ed -L -u "//_:theme/_:name" -v "Onyx" ${HOME}/.config/openbox/lubuntu-rc.xml
 
+# configure bookmarks and lxde theme
+gtk_dir="${HOME}/.config/gtk-3.0"
+mkdir -p ${gtk_dir}
+backup ${gtk_dir}/bookmarks
+backup ${gtk_dir}/settings.ini
+cp -f ${vagrant_files}/gtk/bookmarks ${gtk_dir}
+cp -f ${vagrant_files}/gtk/settings.ini ${gtk_dir}
+
 # create Desktop directory if it doesn't exist
 mkdir -p ${HOME}/Desktop
-
-# add bookmarks
-backup ${HOME}/.gtk-bookmarks
-cp -f ${vagrant_files}/gtk/.gtk-bookmarks ${HOME}
--- scripts/install/install-misc.sh
+++ scripts/install/install-misc.sh
@@ -4,3 +4,6 @@ set -e
 
 # install miscellaneous useful command-line tools
 sudo -E apt-get -qq install lynx
+
+# install useful gui configuration software
+sudo -E apt-get -qq install lxappearance obconf
