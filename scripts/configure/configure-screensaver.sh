c76516fea89d54f313a25ce32d4b65d86adbf5e7
Jacky Xu <jackyxu07@gmail.com>
2018-12-28 22:20:24 -0500

    Disable screensaver properly

--- scripts/configure/configure-screensaver.sh
+++ scripts/configure/configure-screensaver.sh
@@ -5,3 +5,7 @@ set -e
 # uninstall light-locker and xscreensaver
 sudo -E apt-get -qq remove light-locker xscreensaver
 sudo -E apt-get -qq autoremove
+
+# disable power management and screensaver
+backup ${HOME}/.xsessionrc
+cp -f ${vagrant_files}/X/.xsessionrc ${HOME}
