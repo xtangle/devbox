f737c3c66ff7a7389ae36b3afa4ce4ff612f9bbd
Jacky Xu <jackyxu07@gmail.com>
2019-01-08 23:00:32 -0500

    Disable auto upgrades

--- scripts/configure/configure-system.sh
+++ scripts/configure/configure-system.sh
@@ -15,6 +15,12 @@ if id -u ubuntu > /dev/null 2>&1; then
   sudo deluser --remove-home ubuntu
 fi
 
+# disable auto-update since it interferes with provisioning
+sudo bash -c "cat > /etc/apt/apt.conf.d/20auto-upgrades" << EOL
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOL
+
 # update system
 sudo -E apt-get -qq update
 sudo -E apt-get -qq dist-upgrade --fix-missing
