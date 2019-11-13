4bbf7173049cd9b5675fd89e4811ebf615fccf73
Jacky Xu <jackyxu07@gmail.com>
2019-11-13 00:48:15 -0500

    add additional util functions

--- Vagrantfile
+++ Vagrantfile
@@ -53,6 +53,8 @@ Vagrant.configure("2") do |config|
     vb.customize ['modifyvm', :id, '--accelerate3d', 'on']
     vb.customize ['modifyvm', :id, '--clipboard', 'bidirectional']
     vb.customize ['modifyvm', :id, '--draganddrop', 'bidirectional']
+    vb.customize ['modifyvm', :id, '--audioin', 'on']
+    vb.customize ['modifyvm', :id, '--audioout', 'on']
     vb.customize ['modifyvm', :id, '--usbxhci', 'on']
     vb.customize ['modifyvm', :id, '--uartmode1', 'file', File.join(Dir.pwd, 'out/ubuntu-cloudimg-console.log')]
     Provision::Manage::mount_folders vb, provision_vars['mounts']
--- scripts/bootstrap/utils.sh
+++ scripts/bootstrap/utils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function backup {
-  for file in "$@"; do
+  for file in "${@}"; do
     if [[ -f "${file}" && ! -f "${file}.bkup" ]]; then
       sudo cp -af "${file}" "${file}.bkup"
     fi
@@ -21,7 +21,7 @@ function contains {
 }
 
 function load {
-  force=$( [[ "${1}" == "-f" ]] && echo 1 || echo 0 ); (( force )) && shift
+  force=$( [[ "${1}" == "-f" || "${1}" == "--force" ]] && echo 1 || echo 0 ); (( force )) && shift
   file="${1}"
   if (( force )) || [[ -s "${file}" ]]; then
     . "${file}"
@@ -33,7 +33,7 @@ function verlt {
 }
 
 function release_lock_file {
-  force=$( [[ "${1}" == "-f" ]] && echo 1 || echo 0 ); (( force )) && shift
+  force=$( [[ "${1}" == "-f" || "${1}" == "--force" ]] && echo 1 || echo 0 ); (( force )) && shift
   lock_file="${1}"
   echo ">> looking for processes using the lock file: ${lock_file}"
   if [[ ! -f "${lock_file}" ]]; then
@@ -45,23 +45,58 @@ function release_lock_file {
     echo ">> no processes found"
     return
   fi
+  if (( force )); then
+    kill_pids -f "${pids[@]}"
+  else
+    kill_pids "${pids[@]}"
+  fi
+}
+
+function kill_program {
+  force=$( [[ "${1}" == "-f" || "${1}" == "--force" ]] && echo 1 || echo 0 ); (( force )) && shift
+  program="${1}"
+  mapfile -t pids < <(pgrep "${program}")
+  if (( force )); then
+    kill_pids -f "${pids[@]}"
+  else
+    kill_pids "${pids[@]}"
+  fi
+}
+
+function kill_pids {
+  force=$( [[ "${1}" == "-f" || "${1}" == "--force" ]] && echo 1 || echo 0 ); (( force )) && shift
+  pids=("${@}")
+  timeout_secs=30
+  still_running=()
   for pid in "${pids[@]}"; do
     echo ">> killing process: $(ps -p "${pid}" -o comm=) (pid: ${pid})"
     i=0
-    while sudo kill "${pid}" 2>/dev/null; do 
+    while sudo kill "${pid}" 2>/dev/null; do
       sleep 1
-      (( i++ ))
-      if (( i >= 30 )); then
+      (( i++ )) || true
+      if (( i >= timeout_secs )); then
         if (( force )); then
           echo ">> warning: process could not be killed in ${i} seconds, killing process forcefully"
           sudo kill -9 "${pid}"
         else
           echo ">> warning: process could not be killed in ${i} seconds"
+          still_running+=("${pid}")
         fi
         break
       fi
     done
   done
+  if [[ ${#pids[@]} -gt 0 ]]; then
+    echo ">> finished issuing kill command to all processes"
+    if [[ ${#still_running[@]} -gt 0 ]]; then
+      echo ">> warning: the following pids are still running: ${still_running[*]}"
+    fi
+  fi
+}
+function run_in_background {
+  cmd="${1}"
+  # shellcheck disable=2086
+  nohup ${cmd} >/dev/null 2>&1 &
 }
 
 function source_in_profile {
